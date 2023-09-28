f77b92bc49e70406413a3fc9520697366ea06fc0
Arjay Cerio <acerio@lapulapucity.gov.ph>
2023-09-28 11:11:34 +0800

    main title

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lapu-Lapu City Inventory System</title>
     <!-- Custom fonts for this template-->
     <link href="{{ asset("vendor/fontawesome-free/css/all.min.css") }}" rel="stylesheet" type="text/css">
     <link
@@ -16,9 +16,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
-
 </head>
-<body>    <!-- Page Wrapper -->
+<body >  <!-- Page Wrapper -->
     <div id="wrapper">
 
         @include('Layouts.nav')
@@ -41,9 +40,6 @@
     <div class="container-fluid">
         @yield('content')
     </div>
-    
-
-
 
 </div>
 
@@ -57,7 +53,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; create by Briczzz</span>
                     </div>
                 </div>
             </footer>
